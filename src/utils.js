592876be3a12f6e6dc7a3907af31b1a044693684
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-05-30 16:49:47 +0100

    added article comments util function

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,9 @@ export function getArticles(page, limit) {
 export function getTopics() {
   return ncNewsApi.get("/topics").then((res) => res.data);
 }
+
+export function getArticleComments(article_id) {
+  return ncNewsApi
+    .get(`/articles/${article_id}/comments`)
+    .then((res) => res.data);
+}
