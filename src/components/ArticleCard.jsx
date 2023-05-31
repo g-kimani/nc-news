5499b6fe641d7bb383a26d2d117346d114b94904
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-05-31 11:21:44 +0100

    added votes with optimistic rendering

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -9,7 +9,12 @@ import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import ArticleVotes from "./ArticleVotes";
 
-export default function ArticleCard({ article }) {
+export default function ArticleCard({ article, setArticle }) {
+  const setArticleVotes = (votes) => {
+    setArticle((article) => {
+      return { ...article, votes: article.votes + votes };
+    });
+  };
   return (
     <Card sx={{ maxWidth: 345, margin: "5px" }}>
       <CardMedia
@@ -31,11 +36,11 @@ export default function ArticleCard({ article }) {
           <ModeCommentOutlinedIcon />
           {article.comment_count}
         </Button>
-        {/* <Button size="small">
-          <FavoriteBorderOutlinedIcon />
-          {article.votes}
-        </Button> */}
-        <ArticleVotes votes={article.votes} />
+        <ArticleVotes
+          article_id={article.article_id}
+          votes={article.votes}
+          setVotes={setArticleVotes}
+        />
         <Link to={`/articles/${article.article_id}`}>
           <Button>See More...</Button>
         </Link>
--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,11 +1,43 @@
 import { IconButton, Stack, SvgIcon } from "@mui/material";
+import { updateArticle } from "../utils";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import * as React from "react";
 
-export default function ArticleVotes({ votes, setVotes }) {
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function ArticleVotes({ article_id, votes, setVotes }) {
+  const [errorOpen, setErrorOpen] = useState(false);
+  const handleErrorClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setErrorOpen(false);
+  };
+  const handleUpVote = () => {
+    setVotes(1);
+    updateArticle(article_id, { incVotes: 1 }).catch(() => {
+      setVotes(-1);
+      setErrorOpen(true);
+    });
+  };
+  const handleDownVote = () => {
+    setVotes(-1);
+    updateArticle(article_id, { incVotes: -1 }).catch(() => {
+      setVotes(1);
+      setErrorOpen(true);
+    });
+  };
   return (
     <>
       <Stack direction="row" alignItems="center">
         {votes}
-        <IconButton title="up vote">
+        <IconButton title="up vote" onClick={handleUpVote}>
           <SvgIcon>
             <path
               xmlns="http://www.w3.org/2000/svg"
@@ -15,9 +47,8 @@ export default function ArticleVotes({ votes, setVotes }) {
               fill="#fff"
             />
           </SvgIcon>
-          {/* <ThumbUpAltOutlinedIcon /> */}
         </IconButton>
-        <IconButton title="down vote">
+        <IconButton title="down vote" onClick={handleDownVote}>
           <SvgIcon>
             <path
               xmlns="http://www.w3.org/2000/svg"
@@ -30,6 +61,15 @@ export default function ArticleVotes({ votes, setVotes }) {
           </SvgIcon>
         </IconButton>
       </Stack>
+      <Snackbar
+        open={errorOpen}
+        autoHideDuration={3000}
+        onClose={handleErrorClose}
+      >
+        <Alert severity="error">
+          There was an error voting! Please Try again.
+        </Alert>
+      </Snackbar>
     </>
   );
 }
