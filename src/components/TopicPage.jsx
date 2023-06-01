ac8743f2535afeb466341c1f45bb1f61e2643a8d
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-06-01 11:24:03 +0100

    fixed

--- src/components/TopicPage.jsx
+++ src/components/TopicPage.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getArticlesByTopic } from "../utils";
 import ArticleGrid from "./ArticleGrid";
 
 export default function TopicPage() {
